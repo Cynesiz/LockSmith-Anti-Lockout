0f452ba374becd5c7fe6dfb2375afa8c75548369
Riemannian <admin@cynicalsystems.com>
2016-06-24 16:52:50 -0400

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# This script assumes a Debian/Ubuntu Environment
+
+# Update Package Repository
+apt-get update
+
+# Install Required Packages
+apt-get install 
+
+
+
+
+
+
+
+
+
+
+# Allow the application user to access the required commands with sudo
+locksmith ALL=(ALL) !ALL
+locksmith ALL=NOPASSWD: /sbin/reboot
+
+
