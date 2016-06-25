543ecba7ae8b665c230b7573d2db10529189fd68
Riemannian <admin@cynicalsystems.com>
2016-06-24 23:09:41 -0400

    Portability and menu updates
    Replaced echo with printf statements for portability.
    Broke combined menu up into separate submenus.

--- setup.sh
+++ setup.sh
@@ -5,72 +5,68 @@
 # Interface Menus
 clear
 function banner ()
-{
-  echo ""
-  echo -e "    _                _     _____           _ _   _     "
-  echo -e "   | |              | |   / ____|         (_) | | |    " 
-  echo -e "   | |     ___   ___| | _| (___  _ __ ___  _| |_| |__  "
-  echo -e "   | |    / _ \ / __| |/ /\___ \| '_ ` _ \| | __| '_ \ "
-  echo -e "   | |___| (_) | (__|   < ____) | | | | | | | |_| | | |"
-  echo -e "   |______\___/ \___|_|\_\_____/|_| |_| |_|_|\__|_| |_|"
-  echo -e "                                                       "
-  echo ""                                                       
-  echo -e " LockSmith is free software: you can redistribute it and/or modify"
-  echo -e " it under the terms of the GNU General Public License as published by"
-  echo -e " the Free Software Foundation, either version 3 of the License, or"
-  echo -e " (at your option) any later version."
-  echo ""
-  echo -e " LockSmith is distributed in the hope that it will be useful,"
-  echo -e " but WITHOUT ANY WARRANTY; without even the implied warranty of"
-  echo -e " MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the"
-  echo -e " GNU General Public License for more details."
-  echo ""
-  echo -e " You should have received a copy of the GNU General Public License"
-  echo -e " along with LockSmith.  If not, see <http://www.gnu.org/licenses/>."
-  echo ""
+{                                                       
+  line                                                       
+  e " LockSmith is free software: you can redistribute it and/or modify"
+  e " it under the terms of the GNU General Public License as published by"
+  e " the Free Software Foundation, either version 3 of the License, or"
+  e " (at your option) any later version."
+  line
+  e " LockSmith is distributed in the hope that it will be useful,"
+  e " but WITHOUT ANY WARRANTY; without even the implied warranty of"
+  e " MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the"
+  e " GNU General Public License for more details."
+  line
+  e " You should have received a copy of the GNU General Public License"
+  e " along with LockSmith.  If not, see <http://www.gnu.org/licenses/>."
+  line
   enter
 }
+# Pre-set Variables
+runuser="locksmith"
+depends="echoping curl netcat"
+sudoers="/etc/sudoers"
 # Prompt for enter
 function enter ()
 {
-  echo ""
-  read -sn 1 -p "\033[1;33m[!] Press any key to continue..."
+  line
+  read -sn 1 -p "\033[1;33m[!] Press enter to continue..."
   clear
 }
 # Attention
 function attn ()
 {
-  echo -e -n "\033[1;33m[!] $1"
+  printf "%b\n" "\033[1;33m[!] $1"
 }
 # Title
 function title ()
 {
-  echo -e "\n\e[40;38;5;82m $1 \e[0m\n"
+  printf "%b\n" "\n\e[40;38;5;82m $1 \e[0m\n"
 }
-# Confirm Action result=$(confirm(something))
+# Confirm Action result=$(confirm "something")
 # @param string "Confirmation Message"
 # @return bool
 function confirm ()
 {
-   attn($1)
+   attn $1
    read confirm
    # 0 = true; 1 = false;
    if [[ "$confirm" == "y" ]]; then
      unset -v confirm
-     echo 0
+     return 0
    else
      unset -v confirm
-     echo 1
+     return 1
    fi
 }
-# Choose Action result=$(choose(something))
+# Choose Action result=$(choose "something")
 # @param string "Message for choice"
 # @return string "Data entered by user"
 function choose ()
 {
-   attn($1)
+   attn $1
    read choose
-   echo $choose;
+   echo $choose
    unset -v choose
 }
 # Boundry Line
@@ -79,143 +75,123 @@ function choose ()
 function line ()
 {
  case $1 in
-  1 ) echo -e " - - - - - - - - - - - - - - - - - - - - - - - - - - - - ";;
-  2 ) echo -e "---------------------------------------------------------";;
-  3 ) echo -e "\n=========================================================\n";;
-  * ) echo -e "\n";;
+  1 ) printf "%s\n" " - - - - - - - - - - - - - - - - - - - - - - - - - - - - ";;
+  2 ) printf "%s\n" "---------------------------------------------------------";;
+  3 ) printf "\n%s\n" "=========================================================";;
+  * ) printf "\n";;
  esac
 }
 # printf "%s\n" alias
-function e () {
+function e () 
+{
   printf "%s\n" $1
 }
 # Print Main Menu
 function menu_main ()
 {
-   line(2)
+   line 2
    printf "%b\n" "\n\e[30;48;5;82m LOCKSMITH \e[40;38;5;82m Setup and Install \e[0m \n"
-   e("[1] - Semi-Automated Setup: Executes Tasks 2-7"
-   echo "[2] - Program Options and Installation"
-   echo "[3] - Configure Network Testing"
-   echo "[4] - Configure File System Testing"
-   echo "[5] - Configure Lockout Procedures"
-   echo "[6] - Configure Security Options"
-   echo "[7] - Run Configuration Tests"
-   echo "[8] - Send an anonymous thank you"
-   line()
-   echo "[0] - Exit Program."
-   line()
-   echo "Enter selection: "
-}
-
-   echo -e "\n\e[40;38;5;82m File System Testing  \e[0m \n"
-    
-   echo -e "\n\e[40;38;5;82m Lockout Procedures \e[0m \n"
-   echo "[12] - Setup Push Notifications"
-   echo "[13] - Setup IPTables Emergency Procedure"
-   echo "[14] - Setup Network Interface Emergency Procedure"
-   echo "[15] - Setup Daemon Restart Procedure(s)"
-   echo "[16] - Setup System Reboot Procedure"
-   echo "[17] - Setup Reverse Connect Procedure"
-   echo "[18] - Setup Custom Procedure(s)"
-   echo "[19] - Setup Last Resort Procedures"
-   echo ""
-   echo "[20] - Let us know you found this useful (anonymous)"
-   echo ""
-   echo "[0] - exit program."
-   echo ""
-   echo "Enter selection: "
+   e "[1] - (Semi) Automated Setup: Executes Tasks 2-8"
+   e "[2] - Program Options and Installation"
+   e "[3] - Configure Network Testing"
+   e "[4] - Configure File System Testing"
+   e "[5] - Configure Lockout Procedures"
+   e "[6] - Configure Security and Logging"
+   e "[7] - Test Current Configuation"
+   e "[8] - Send an anonymous thank you"
+   line
+   e "[0] - Exit Program."
+   line
+   e "Enter selection: "
 }
 # Print Install menu
 function menu_install ()
 {
-   e("[1] - Update Repositories")
-   e("[2] - Install Dependencies")
-   e("[3] - Define Lockout Levels")
-   e("[4] - Add LockSmith User (nologin)"
-   e("[5] - Allow Priviledged Commands")
-   e("[6] - Install LockSmith")
-   e("[7] - Add Crontab Entry")
-   e("[8] - Additional Options")    
-   line()
-   e("[0] - Previous Menu.")
-   line()
-   e("Enter selection: ")
+   e "[1] - Update Repositories"
+   e "[2] - Install Dependencies"
+   e "[3] - Define Lockout Levels"
+   e "[4] - Add LockSmith User (nologin)"
+   e "[5] - Allow Priviledged Commands"
+   e "[6] - Install LockSmith"
+   e "[7] - Add Crontab Entry"
+   e "[8] - Additional Options"    
+   line
+   e "[0] - Previous Menu."
+   line
+   e("Enter selection: "
 }
 # Print Network Menu
 function menu_network ()
 {
-   echo ""
-   echo -e "\n\e[40;38;5;82m Network Testing  \e[0m \n"
-   echo "[1] - Confirgure Network Tests"
-   echo "[2] - Choose Server for Ingress testing"
-   echo "[3] - Open Egress Port for Network Testing"
-   echo ""
-   echo "[0] - Previous Menu."
-   echo ""
-   "Enter selection: "
-}   
-
-function lockout_menu ()
+   line 2
+   printf "%b\n" "\n\e[40;38;5;82m Network Configuration  \e[0m"
+   e "[1] - Network Lockout Levels"
+   e "[2] - Builtin Network Tests"
+   e "[3] - Custom Network Tests (expert)"
+   e "[4] - Set Server for Ingress Testing"
+   e "[5] - Open Egress Port for Testing (Important)"
+   e "[6] - IPTables Emergency Procedure"
+   e "[7] - Network Interface(s) Emergency Procedure"
+   e "[8] - Network Route Emergency Procedure"
+   line
+   e "[0] - Previous Menu."
+   line
+   choose "Enter selection: "
+} 
+# File system tests menu
+function filesystem_menu ()
 {
-   echo -e "\n\e[40;38;5;82m Lockout Procedures \e[0m \n"
-   echo "[1] - Setup Push Notifications"
-   echo "[2] - Setup IPTables Emergency Procedure"
-   echo "[3] - Setup Network Interface Emergency Procedure"
-   echo "[4] - Setup Daemon Restart Procedure(s)"
-   echo "[5] - Setup System Reboot Procedure"
-   echo "[6] - Setup Reverse Connect Procedure"
-   echo "[7] - Setup Custom Procedure(s)"
-   echo "[8] - Setup Last Resort Procedures"
-   echo ""
-   echo "[0] - Previous Menu."
-   echo ""
-   "Enter selection: "
+   line 2
+   printf "%b\n" "\n\e[40;38;5;82m File System Configuration  \e[0m"
+   e "[1] - FS Lockout Levels"
+   e "[2] - FS Builtin Tests"
+   e "[3] - FS Custom Tests (expert)"
+   e "[4] - FS Lockout Procedures"
+   line
+   e "[0] - Previous Menu."
+   line
+   choose "Enter selection: "
+}
+# Lockout Procedures Menu
+function lockout_menu ()
+{    
+   line 2
+   printf "%b\n" "\n\e[40;38;5;82m Lockout Procedures \e[0m"
+   e "[1] - Define Lockout Levels"
+   e "[2] - Push Notifications"
+   e "[3] - IPTables Emergency Procedure"
+   e "[4] - Network Interface Emergency Procedure"
+   e "[5] - Network Route Emergency Procedure"
+   e "[6] - Daemon Restart Procedure"
+   e "[7] - System Reboot Procedure"
+   e "[8] - Reverse Connect Procedure"
+   e "[9] - Last Resort Procedure"
+   e "[10] - Custom Procedures (expert)"
+   line
+   e "[0] - Previous Menu."
+   line
+   choose "Enter selection: "
+}
+# Security and Logging Menu
+function security_menu()
+{
+ # Add hash checking
+ # Add logging
 }
 
-# green bg/black fg \e[30;48;5;82m
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# Begin Program
 banner
-
 selection=
  until [ "$selection" = "0" ]; do
    print_menu
    read selection
-   echo ""
+   line
    case $selection in
-
-   1 ) apt update -y;;
-   2 ) apt install ${depends};;
-   3 ) 
+   1 ) autoinstall;;
+   2 ) apt-get update;;
+   3 ) apt-get install ${depends} -y;;
    4 ) 
    5 ) 
    6 ) 
@@ -223,41 +199,11 @@ selection=
    8 ) 
    9 ) 
    10 ) 
-   11 ) 
-   12 ) 
-   13 ) 
-   14 ) 
-   15 ) 
-   16 ) 
-   17 ) 
-   18 ) 
-   19 ) 
-   0 ) 
-   * ) 
+   0 ) exit;;
+   * ) clear;menu_main;;
    esac
  done
 
-
-
-
-# Pre-set Variables
-runuser="locksmith"
-depends=""
-sudoers="/etc/sudoers"
-
-
-
-
-# Update Package Repository
-apt-get update
-
-# Install Required Packages
-apt-get install 
-
-
-
-
- 
 function sudoconfig ()
 {
    # Add a restricted user for LockSmith 
@@ -266,11 +212,12 @@ function sudoconfig ()
    # Backup Sudoers File
    cp /etc/sudoers /etc/sudoers.bak
 
-   # Allow the user to access the required commands with sudo
-   cat >>/etc/sudoers <<<EOL
+   # Allow locksmith access to privileged commands
+   # Writes to sudoers file
+   cat >>${sudoers} <<<EOL
 # LockSmith Anti-Lockout Privileged Commands
 # User alias specification
-User_Alias      LOCKSMITH = locksmith
+User_Alias      LOCKSMITH = ${runuser}
 # Command alias specification
 Cmnd_Alias      CMD_REBOOT = /sbin/reboot
 Cmnd_Alias      CMD_IPTABLES = /sbin/iptables
@@ -282,7 +229,7 @@ Cmnd_Alias      CMD_KILLALL = /usr/bin/killall
 Cmnd_Alias      CMD_MOUNT = /bin/mount
 Cmnd_Alias      CMD_UMOUNT = /bin/umount
 Cmnd_Alias      CMD_FSCK = /sbin/fsck
-Cmnd_Alias      CMD_ = 
+#Cmnd_Alias      CMD_ = 
 #Cmnd_Alias      CMD_ = 
 # User privilege specification
 LOCKSMITH ALL=(ALL) !ALL
@@ -296,7 +243,7 @@ LOCKSMITH ALL=NOPASSWD: CMD_KILLALL
 #LOCKSMITH ALL=NOPASSWD: CMD_
 # End LockSmith Sudoers Config
 EOL
-
+}
 
 
 
